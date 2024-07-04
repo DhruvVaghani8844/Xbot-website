b0689feabf2cb82de95284be42a2f92d02dd17a1
DhruvVaghani8844 <vaghani.dhruv41@gmail.com>
2024-07-04 21:12:14 +0530

    xbot

--- src/Components/HeaderSection/HeaderSection.jsx
+++ src/Components/HeaderSection/HeaderSection.jsx
@@ -25,36 +25,7 @@ const HeaderSection = () => {
             </button> */}
             <button className="site-button d-none d-sm-flex">
               Get Early Access
-              <section className="centered-container">
-                <a className="link link--arrowed">
-                  <svg
-                    className="arrow-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={22}
-                    height={22}
-                    viewBox="0 0 32 32"
-                  >
-                    <g
-                      fill="none"
-                      stroke="#fff"
-                      strokeWidth="1.5"
-                      strokeLinejoin="round"
-                      strokeMiterlimit={10}
-                    >
-                      <circle
-                        className="arrow-icon--circle"
-                        cx={16}
-                        cy={16}
-                        r="15.12"
-                      />
-                      <path
-                        className="arrow-icon--arrow"
-                        d="M16.14 9.93L22.21 16l-6.07 6.07M8.23 16h13.98"
-                      />
-                    </g>
-                  </svg>
-                </a>
-              </section>
+              
             </button>
             <div className="d-block d-sm-none">
               <section className="site-header_small">
