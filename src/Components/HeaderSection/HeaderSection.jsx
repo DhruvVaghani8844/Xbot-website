e0bf5eec779b9fdd4c2251635e2e083893b43692
DhruvVaghani8844 <vaghani.dhruv41@gmail.com>
2024-07-09 11:32:17 +0530

    xbot

--- src/Components/HeaderSection/HeaderSection.jsx
+++ src/Components/HeaderSection/HeaderSection.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeaderSection.css";
 import { xbot_logo } from "../../assets/index.js";
 
-const HeaderSection = ({scrollToSection, refs }) => {
+const HeaderSection = ({ scrollToSection, refs }) => {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  const closeMobileNav = () => {
+    setIsMobileNavOpen(false);
+  };
+
+  const handleNavLinkClick = (sectionRef) => {
+    scrollToSection(sectionRef);
+    closeMobileNav();
+  };
+
   return (
     <div className="xbot-container">
       <div className="site-header_placeholder"></div>
@@ -15,21 +30,62 @@ const HeaderSection = ({scrollToSection, refs }) => {
               </a>
             </div>
             <nav className="site_header-mainNav d-none d-sm-flex">
-              <a  onClick={() => scrollToSection(refs.featuresRef)}>Features</a>
-              <a  onClick={() => scrollToSection(refs.aboutRef)}>About</a>
-              <a href="">Blog</a>
+              <a onClick={() => handleNavLinkClick(refs.featuresRef)}>Features</a>
+              <a onClick={() => handleNavLinkClick(refs.aboutRef)}>About</a>
+              <a href="#">Blog</a>
             </nav>
-            {/* <button className="site-button d-none d-sm-flex">
-              {" "}
-              Get Early Access
-            </button> */}
             <button className="site-button d-none d-sm-flex">
               Get Early Access
-              
             </button>
             <div className="d-block d-sm-none">
               <section className="site-header_small">
-                <button className="site-header_menuBtn">Menu</button>
+                <button className="site-header_menuBtn">
+                  <label className={`hamburger ${isMobileNavOpen ? "open" : ""}`}>
+                    <input type="checkbox" />
+                    <svg viewBox="0 0 32 32" onClick={toggleMobileNav}>
+                      <path
+                        className="line line-top-bottom"
+                        d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                      ></path>
+                      <path className="line" d="M7 16 27 16"></path>
+                    </svg>
+                  </label>
+                </button>
+              </section>
+              <section
+                className={`site-header_mobileNav ${
+                  isMobileNavOpen ? "open" : ""
+                }`}
+              >
+                <div className="xbot-container">
+                  <nav className="site-header_mainMobileNav">
+                    <ul>
+                      <li>
+                        <a href="#">Blog</a>
+                      </li>
+                      <li>
+                        <a onClick={() => handleNavLinkClick(refs.aboutRef)}>
+                          About
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={() => handleNavLinkClick(refs.featuresRef)}>
+                          Features
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <nav className="site-header_mainMobileNav site-header_secondary">
+                    <ul>
+                      <li>
+                        <a href="#">Terms of service</a>
+                      </li>
+                      <li>
+                        <a href="#">Privacy policy</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
               </section>
             </div>
           </div>
